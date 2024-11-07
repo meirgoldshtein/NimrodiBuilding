dfd4b2ef99ce2170ac3ac07f531dd7b83d7991d5
meirgoldshtein <m0548491057@gmail.com>
2024-11-07 12:31:15 +0200

    Added a private component that secures protected components to authorized users

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,13 +1,26 @@
 import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
+import Forbidden from "../pages/Forbidden/Forbidden";
 
 interface IPrivateRoute{
     component: ReactNode,
 }
 
 const PrivateRoute = ({ component}:IPrivateRoute) => {
-    //FILL HERE 3.6
+    const currentRole = useSelector((state: { role: string }) => state.role);
+    const access = useSelector((state: { floorAccess: { floorAccess: [boolean, boolean, boolean, boolean, boolean] } }) => state.floorAccess.floorAccess);
+    const { index } = useParams<{ index: string }>();
+
+    if (!access[parseInt(index || "0")]) {
+      return <Forbidden />;
+    }
+    
+    if (access[parseInt(index || "0")]) {
+      return component;
+    } else {
+      return <Forbidden />
+    }
 };
 
 export default PrivateRoute
